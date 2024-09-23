cc8a16a3d9c8060ba7702d2a94eee64327a2c0b3
priyanshu <priyanshusoni0927@gmail.com>
2024-09-23 19:02:56 +0530

    modified ToDo CLI

--- week-4/4.1_NodeJS/index.js
+++ week-4/4.1_NodeJS/index.js
@@ -1,24 +1,24 @@
-// // const fs = require("fs");
+// const fs = require("fs");
 
-// // function main(fileName){
-// //     fs.readFile(fileName, "utf-8",function(err,data){
-// //         let total =0;
-// //         for(let i=0;i<data.length;i++){
-// //             if(data[i]===" "){
-// //                  total++;
+// function main(fileName){
+//     fs.readFile(fileName, "utf-8",function(err,data){
+//         let total =0;
+//         for(let i=0;i<data.length;i++){
+//             if(data[i]===" "){
+//                  total++;
 
-// //             }
-// //         }
+//             }
+//         }
 
-// //         console.log(total);
-// //     })
-
-
-
-// // }
+//         console.log(total);
+//     })
 
 
-// // main(process.argv());
+
+// }
+
+
+// main("./a.txt");
 
 
 //****************/ using comand line commander library*****************************/
